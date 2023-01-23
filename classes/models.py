ce224bb4fc68f43eecfbd5c3504873a325b31327
mallollaa <mah41411@gmail.com>
2023-01-23 19:49:12 +0300

    Done

--- classes/models.py
+++ classes/models.py
@@ -3,10 +3,12 @@ from django.db import models
 
 class Course(models.Model):
     name = models.CharField(max_length=30)
-
+    
 
 class Lecture(models.Model):
     name = models.CharField(max_length=30)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE ,related_name="lectures")
+    
 
     def __str__(self):
         return self.name
@@ -15,6 +17,7 @@ class Lecture(models.Model):
 class Slide(models.Model):
     name = models.CharField(max_length=30)
     link = models.URLField()
+    Lecture = models.OneToOneField(Lecture , on_delete=models.CASCADE , related_name="slide", primary_key=True)
 
     def __str__(self):
         return self.name
@@ -23,13 +26,14 @@ class Slide(models.Model):
 class Assignment(models.Model):
     name = models.CharField(max_length=30)
     link = models.URLField()
-
+    Lecture = models.OneToOneField(Lecture , on_delete=models.CASCADE , related_name="slide", primary_key=True)
     def __str__(self):
         return self.name
 
 
 class Tag(models.Model):
     name = models.CharField(max_length=30)
+    courses = models.ManyToManyField(Course, on_delete=models.CASCADE ,related_name='tags')
 
     def __str__(self):
         return self.name
